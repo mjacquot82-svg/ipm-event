5785dfef9d7c9f7ba5bd5c2abd5f088eff36e783
mjacquot82-svg <mjacquot82@gmail.com>
2026-03-29 09:03:44 -0400

    Rename PreviewPage to PreviewRoot

--- frontend/app/preview.tsx
+++ frontend/app/preview.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from 'expo-router';
 
-export default function PreviewPage() {
-  // This sends anyone at /preview straight to the 2026 content
+export default function PreviewRoot() {
   return <Redirect href="/preview-2026" />;
 }
