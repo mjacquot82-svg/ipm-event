f6ada392149cf1d4d35489b90b45ec1028a533c5
mjacquot82-svg <mjacquot82@gmail.com>
2026-03-29 13:38:10 -0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -21,10 +21,7 @@ import json
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-# MongoDB connection
-mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+
 
 # Google Sheet URLs (public CSV export)
 EVENTS_SHEET_ID = "1tnfBd7Ffg5S4hyk5c5CpB-VGkJcSnLpdsKGbNJIiQCs"
