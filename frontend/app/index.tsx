93a5481f649803d608124498b08c6daf4abcf03c
mjacquot82-svg <mjacquot82@gmail.com>
2026-03-29 09:05:33 -0400

    Rename PreviewPage to RootIndex and clean imports

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,8 @@
 // © 2026 1001538341 ONTARIO INC. All Rights Reserved.
 // Root index - Redirects to Coming Soon page
-
-import { Redirect, useSegments } from 'expo-router';
-
 import { Redirect } from 'expo-router';
 
-export default function PreviewPage() {
-  // This sends anyone at /preview straight to the 2026 content
+export default function RootIndex() {
+  // This sends everyone straight to the 2026 content
   return <Redirect href="/preview-2026" />;
 }
