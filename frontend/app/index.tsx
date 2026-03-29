1e3044200c1f6871116cbac20f12c51dd61abd22
mjacquot82-svg <mjacquot82@gmail.com>
2026-03-29 09:08:39 -0400

    Update index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -3,6 +3,5 @@
 import { Redirect } from 'expo-router';
 
 export default function RootIndex() {
-  // This sends everyone straight to the 2026 content
   return <Redirect href="/preview-2026" />;
 }
